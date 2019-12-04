af6a27fa4a9293d1d0e4231ce24d300149725121
aereal <aereal@aereal.org>
2019-12-04 17:08:13 +0900

    consumer

---
+++ consumer.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+const jwt = require('jsonwebtoken');
+const fs = require('fs');
+
+const bufSize = 4096;
+const buf = Buffer.alloc(bufSize);
+let token = '';
+while (true) {
+  const hasRead = fs.readSync(0, buf, 0, bufSize);
+  if (hasRead <= 0) break;
+  token += buf.slice(0, hasRead).toString('utf8');
+}
+
+const publicKey = fs.readFileSync('./keys/public-key.pem', 'utf8');
+const decoded = jwt.verify(token.trim(), publicKey, { algorithms: ['RS256'] });
+process.stdout.write(JSON.stringify(decoded) + "\n")
